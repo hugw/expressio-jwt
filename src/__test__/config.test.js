42977e3d905d9f1186013e6606b49a64b5e4b43d
Hugo W <hugwnt@gmail.com>
2019-07-07 09:47:53 -0400

    Make secret key explicitly required

--- src/__test__/config.test.js
+++ src/__test__/config.test.js
@@ -8,7 +8,7 @@ describe('Expressio JWT / Configs', () => {
           enabled: true,
           expiresIn: '7d',
           algorithm: 'HS256',
-          secret: process.env.SECRET || 'cAQk{m04|:b&MCkD2T0S3C!Da$dko7{EN/gtoH{UO:EM`zdGc-~O>U@$yhz.UDA',
+          secret: null,
         },
       },
       test: {},
--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ export default {
       enabled: true,
       expiresIn: '7d',
       algorithm: 'HS256',
-      secret: process.env.SECRET || 'cAQk{m04|:b&MCkD2T0S3C!Da$dko7{EN/gtoH{UO:EM`zdGc-~O>U@$yhz.UDA',
+      secret: null,
     },
   },
 
